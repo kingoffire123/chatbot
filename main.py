a4cb030b29b90b57f00124aee75690363daa5fce
Pierre-Étienne Mélet <pem@google.com>
2025-08-17 18:52:20 +0200

    squelette

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+lune = """La Lune[a], ou Terre I[b], est l'unique satellite naturel permanent de la planète Terre. Il s'agit du cinquième plus grand satellite naturel du Système solaire, et du plus grand des satellites planétaires par rapport à la taille de la planète autour de laquelle il orbite. C'est le deuxième satellite le plus dense du Système solaire après Io, un satellite de Jupiter[c].
+La Lune est en rotation synchrone avec la Terre, lui montrant donc constamment la même face. Celle-ci, appelée face visible, est marquée par des mers lunaires volcaniques sombres qui remplissent les espaces entre les hautes terres claires et ses cratères d'impact proéminents. Réciproquement, elle possède une face cachée, qui présente moins de mers mais beaucoup plus de cratères, dont le bassin Pôle Sud-Aitken, le plus grand du satellite et l'un des plus grands du Système solaire par son diamètre de 2 500 km. Elle est dépourvue d'atmosphère dense et de champ magnétique. Son influence gravitationnelle sur la Terre produit les marées océaniques, les marées terrestres, un léger allongement de la durée du jour et la stabilisation de l'inclinaison de l'axe terrestre.
+La distance orbitale moyenne de la Lune est de 384 402 km, soit environ trente fois le diamètre terrestre, et sa période de révolution vaut 27,3 jours. La taille apparente de la Lune dans le ciel est approximativement la même que celle du Soleil, puisque le diamètre de l'étoile est environ 400 fois celui du satellite, mais qu'elle est également 400 fois plus éloignée. Par conséquent, la Lune peut couvrir presque exactement le Soleil dans le ciel, permettant l'apparition d'éclipses solaires totales. Cette correspondance de taille apparente disparaîtra dans un avenir lointain du fait de l'augmentation de la distance lunaire d'environ 3,8 cm par an. La formation de la Lune remonterait à environ 4,51 milliards d'années, peu de temps après celle de la Terre. L'explication la plus largement acceptée est que la Lune s'est formée à partir des débris restants après un impact géant entre une proto-Terre et une protoplanète de la taille de Mars, appelée Théia.
+La Lune est survolée pour la première fois par la sonde spatiale Luna 2 en 1959. Durant plus d'une décennie, elle est notamment étudiée par les programmes Luna et Apollo, respectivement soviétique et américain. Cette course à l'espace culmine en 1969 avec les premiers humains posant le pied sur la Lune lors de la mission Apollo 11 emportant Neil Armstrong et Buzz Aldrin. Dix autres astronautes de la NASA foulent ensuite le sol lunaire jusqu'à Apollo 17 en 1972. Ces missions permettent de ramener sur Terre des roches lunaires qui, avec les observations effectuées sur place, permettent de développer la connaissance géologique de la Lune, de sa structure interne et de l'histoire de sa formation. Délaissée à partir de 1974 par les puissances spatiales, l'astre connaît un nouvel intérêt dans les années 1990, deux missions de la NASA — Clementine et Lunar Prospector — découvrant des indices de la présence de glace d'eau, notamment au pôle Sud. À compter de la fin des années 1990, la Lune est la destination principale des sondes spatiales des nouvelles nations spatiales, notamment la Chine, le Japon et l'Inde. De nouvelles missions habitées vers la Lune, voire une colonisation, sont envisagées dans les années 2020.
+En sa qualité de deuxième objet céleste du ciel terrestre par sa magnitude apparente, après le Soleil, et du fait de son cycle régulier de phases correspondant à sa période synodique de 29,5 jours, la Lune sert de référence et d'influence culturelle aux sociétés humaines depuis des temps immémoriaux. Celles-ci se retrouvent dans la langue, les calendriers, l'art et la mythologie.
+"""
 
 
-# Press the green button in the gutter to run the script.
+# cette fonction charge les articles les uns apres les autres
+# et extrait les mots-cles.
+def charger_et_preparer():
+    # charger un article en memoire
+    # decouper l'article en phrases
+    # pour chaque phrase, decoupe en mots
+    # compter tous les mots
+    pass
+
+def question_et_repondre():
+    pass
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # 1. charger les articles et preparer les reponses
+    charger_et_preparer()
+    # 2. poser des questions et repondre
+    question_et_repondre()
