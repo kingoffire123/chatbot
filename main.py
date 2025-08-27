9ce756ef6bc6fb71b1893fa1a4eb3cafe87c3438
Pierre-Etienne Mélet <melet.pierre.etienne@gmail.com>
2025-08-28 00:30:13 +0200

    prototype

--- main.py
+++ main.py
@@ -1,27 +1,98 @@
+import re
+from collections import defaultdict
+from collections import Counter
 
+mots_communs = set([
+    'de', 'la', '', 'et', 'le', 'est', 'du', 'les', 'des', 'plus', 'par', 'a', 'à', 'sa', 'dans',
+    'un', 'après', 'en', 'une', 'sur', 'fois', 'grand', 'avec', 'même', 'qui', 'entre', 'elle', 'mais',
+    'son', 'que','celle', 'cette', 'fait', 'ou', 'il', "c'est", 'lui', 'donc', 'celle-ci', "d'une",
+    "quel", "quelle"
+])
 
-lune = """La Lune[a], ou Terre I[b], est l'unique satellite naturel permanent de la planète Terre. Il s'agit du cinquième plus grand satellite naturel du Système solaire, et du plus grand des satellites planétaires par rapport à la taille de la planète autour de laquelle il orbite. C'est le deuxième satellite le plus dense du Système solaire après Io, un satellite de Jupiter[c].
-La Lune est en rotation synchrone avec la Terre, lui montrant donc constamment la même face. Celle-ci, appelée face visible, est marquée par des mers lunaires volcaniques sombres qui remplissent les espaces entre les hautes terres claires et ses cratères d'impact proéminents. Réciproquement, elle possède une face cachée, qui présente moins de mers mais beaucoup plus de cratères, dont le bassin Pôle Sud-Aitken, le plus grand du satellite et l'un des plus grands du Système solaire par son diamètre de 2 500 km. Elle est dépourvue d'atmosphère dense et de champ magnétique. Son influence gravitationnelle sur la Terre produit les marées océaniques, les marées terrestres, un léger allongement de la durée du jour et la stabilisation de l'inclinaison de l'axe terrestre.
-La distance orbitale moyenne de la Lune est de 384 402 km, soit environ trente fois le diamètre terrestre, et sa période de révolution vaut 27,3 jours. La taille apparente de la Lune dans le ciel est approximativement la même que celle du Soleil, puisque le diamètre de l'étoile est environ 400 fois celui du satellite, mais qu'elle est également 400 fois plus éloignée. Par conséquent, la Lune peut couvrir presque exactement le Soleil dans le ciel, permettant l'apparition d'éclipses solaires totales. Cette correspondance de taille apparente disparaîtra dans un avenir lointain du fait de l'augmentation de la distance lunaire d'environ 3,8 cm par an. La formation de la Lune remonterait à environ 4,51 milliards d'années, peu de temps après celle de la Terre. L'explication la plus largement acceptée est que la Lune s'est formée à partir des débris restants après un impact géant entre une proto-Terre et une protoplanète de la taille de Mars, appelée Théia.
-La Lune est survolée pour la première fois par la sonde spatiale Luna 2 en 1959. Durant plus d'une décennie, elle est notamment étudiée par les programmes Luna et Apollo, respectivement soviétique et américain. Cette course à l'espace culmine en 1969 avec les premiers humains posant le pied sur la Lune lors de la mission Apollo 11 emportant Neil Armstrong et Buzz Aldrin. Dix autres astronautes de la NASA foulent ensuite le sol lunaire jusqu'à Apollo 17 en 1972. Ces missions permettent de ramener sur Terre des roches lunaires qui, avec les observations effectuées sur place, permettent de développer la connaissance géologique de la Lune, de sa structure interne et de l'histoire de sa formation. Délaissée à partir de 1974 par les puissances spatiales, l'astre connaît un nouvel intérêt dans les années 1990, deux missions de la NASA — Clementine et Lunar Prospector — découvrant des indices de la présence de glace d'eau, notamment au pôle Sud. À compter de la fin des années 1990, la Lune est la destination principale des sondes spatiales des nouvelles nations spatiales, notamment la Chine, le Japon et l'Inde. De nouvelles missions habitées vers la Lune, voire une colonisation, sont envisagées dans les années 2020.
-En sa qualité de deuxième objet céleste du ciel terrestre par sa magnitude apparente, après le Soleil, et du fait de son cycle régulier de phases correspondant à sa période synodique de 29,5 jours, la Lune sert de référence et d'influence culturelle aux sociétés humaines depuis des temps immémoriaux. Celles-ci se retrouvent dans la langue, les calendriers, l'art et la mythologie.
-"""
-
+def decoupe_phrase(phrase):
+    # on retire les espaces au debut et a la fin
+    phrase = phrase.strip(" \n\t.?")
+    # on retire les characteres pas interessants
+    phrase = phrase.replace("\n", "")
+    # si la phrase est vide, on passe a la suivante
+    if len(phrase) == 0:
+        return None
+    # on retire les majuscules
+    phrase = phrase.lower()
+    # pour chaque phrase, on decoupe en mots
+    mots = re.split("[ ,;:'()]", phrase)
+    # on retire les mots trop communs
+    return set(mots) - mots_communs
 
 # cette fonction charge les articles les uns apres les autres
 # et extrait les mots-cles.
-def charger_et_preparer():
-    # 1. charger un article en memoire
-    # 2. decouper l'article en phrases
-    # 3. pour chaque phrase, decoupe en mots
-    # 4. compter tous les mots
-    pass
+def charger_et_preparer(fichiers):
+    phrases = [] 
+    index_phrase = 0
+    phrases_par_mot = defaultdict(set)
+    for fichier in fichiers:
+        # 1. charger un article en memoire
+        with open(fichier) as f:
+            texte = f.read()
+            # 2. decouper l'article en phrases
+            texte_phrases = re.split("[.\n]", texte)
+            # 3. pour chaque phrase, decoupe en mots
+            tous_les_mots = Counter()
+            for phrase in texte_phrases:
+                mots = decoupe_phrase(phrase)
+                if not mots: continue
+                phrases.append(phrase)
+                for mot in mots:
+                    # pour chaque mot, on garde la liste des phrases
+                    phrases_par_mot[mot].add(index_phrase)
+                index_phrase += 1 
+    # on ne va garder que les mots qui apparaissent dans moins de 50% des phrases
+    seuil = index_phrase/2
+    for mot in phrases_par_mot:
+        if len(phrases_par_mot[mot]) > seuil:
+            print ("Oublie",mot)
+            phrases_par_mot[mot] = None
+    return phrases_par_mot, phrases
 
-def question_et_repondre():
-    pass
+def meilleures_reponses(question, phrases_par_mot, phrases, nombre):
+    # on prend les mots de la phrase
+    mots = decoupe_phrase(question)
+    
+    # Puis on retrouve toutes les phrases.
+    p = []
+    for mot in mots:
+        s = phrases_par_mot[mot]
+        if s is not None and len(s) > 0:
+            # a la fin, la liste comprend toutes les phrases, parfois en plusieurs copies si elles
+            # correspondent a plusieurs mots
+            p.extend(list(s))
+            #print (mot,s)
+
+    # on compte les phrases qu'on a trouve
+    reponses = Counter(p)
+
+    # et on garde les meilleures
+    resultat = []
+    for r in reponses.most_common(nombre):
+        phrase, frequence = r
+        resultat.append((phrase, frequence, phrases[phrase]))
+    return  resultat
+    
 
 if __name__ == '__main__':
     # 1. charger les articles et preparer les reponses
-    charger_et_preparer()
+    phrases_par_mot, phrases = charger_et_preparer([
+        "lune.txt",
+        "terre.txt",
+        "mars.txt",
+        "systeme_solaire.txt"
+    ])
     # 2. poser des questions et repondre
-    question_et_repondre()
+    questions = [
+        "Quelle est la masse de la lune par rapport a celle de la terre ?",
+        "Quel est l'ordre des planètes dans le système solaire ?"
+    ]
+    for q in questions:
+        print ("\n>",q)
+        for r in meilleures_reponses(q, phrases_par_mot, phrases, 2):
+            print (r)
